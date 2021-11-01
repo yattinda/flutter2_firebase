98a6f1edbb2426cf5947417fcc1f71b9fc3d8f41
yattinda <yattinda@gmail.com>
2021-11-01 15:32:53 +0900

    Import Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
   // 最初に表示するWidget
